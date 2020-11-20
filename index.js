c4f6533627527d7c7898ce9222646dcfc93b7d55
markussim <markus.simonsen@gmail.com>
2020-11-20 12:59:57 +0100

    Added other metrics and fixed performence issues

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const { on } = require("process");
-var os = require('os-utils');
+var os = require("os-utils");
+var os2 = require("os");
+const disk = require("diskusage");
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+let path = os2.platform() === "win32" ? "c:" : "/";
+
 io.on("connection", (socket) => {
   console.log("Got connect!");
 
-  setInterval(() => {
-    os.cpuUsage(function(v){
-      console.log( 'CPU Usage (%): ' + (v * 100).toFixed(1) );
+  let interval = setInterval(() => {
+    os.cpuUsage(function (v) {
       socket.emit("cpu", (v * 100).toFixed(1));
     });
-    
+    socket.emit("mem", ((os2.freemem / os2.totalmem) * 100).toFixed(1));
+    disk
+      .check(path)
+      .then((info) =>
+        socket.emit("disk", ((info.free / info.total) * 100).toFixed(1))
+      )
+      .catch((err) => console.error(err));
   }, 300);
 
   socket.on("msg", function (data) {
     console.log(data);
-    socket.emit("cpu", "hello friends!");
+    socket.emit("cpu", "null");
   });
 
   socket.on("disconnect", function () {
+    clearInterval(interval);
     console.log("Got disconnect!");
   });
 });
